d25108122e0e10131e4752a93ab19a647fb0c189
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-26 18:26:20 +0300

    add more states

--- routers/survey/states.py
+++ routers/survey/states.py
@@ -1,7 +1,29 @@
+from enum import StrEnum
+
 from aiogram.fsm.state import StatesGroup, State
 
 
 class Survey(StatesGroup):
     full_name = State()
     email = State()
+    sport = State()
     email_newsletter = State()
+
+
+class SurveySportDetails(StatesGroup):
+    tennis = State()
+    football = State()
+    formula_one = State()
+
+
+class KnownSports(StrEnum):
+    tennis = "Tennis"
+    football = "Football"
+    formula_one = "Formula One"
+
+
+class KnownF1Tracks(StrEnum):
+    monaco = "Monaco"
+    spa = "Spa"
+    suzuka = "Suzuka"
+    monza = "Monza"
