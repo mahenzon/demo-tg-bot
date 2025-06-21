308d36f40d2caa895e0d03da230d2ebaeaec8e0c
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-06-21 15:08:22 +0300

    accept data from middleware into handler

--- routers/admin_handlers.py
+++ routers/admin_handlers.py
@@ -1,9 +1,11 @@
 from re import Match
 
 from aiogram import Router, F, types
+from aiogram.utils import markdown
 from magic_filter import RegexpMode
 
 from config import settings
+from middlewares.rate_limit_middleware import RateLimitInfo
 
 router = Router(name=__name__)
 
@@ -17,5 +19,18 @@ async def secret_admin_message(message: types.Message):
     F.from_user.id.in_(settings.admin_ids),
     F.text.regexp(r"(\d+)", mode=RegexpMode.MATCH).as_("code"),
 )
-async def handle_code(message: types.Message, code: Match[str]):
-    await message.reply(f"Your code: {code.group()}")
+async def handle_code(
+    message: types.Message,
+    code: Match[str],
+    rate_limit_info: RateLimitInfo,
+    # rate_limit_info: RateLimitInfo | None = None,
+):
+    count = rate_limit_info.message_count
+    first_msg = rate_limit_info.first_message
+    text = markdown.text(
+        f"Your code: {code.group()}\n",
+        f"RL messages count: {count}",
+        f"RL first message: {first_msg}",
+        sep="\n",
+    )
+    await message.reply(text=text)
