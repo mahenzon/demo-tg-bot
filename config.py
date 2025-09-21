488ef0673d80559d131d2529ed58f46a8b028328
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-09-21 18:29:38 +0300

    update config

--- config.py
+++ config.py
@@ -4,10 +4,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         case_sensitive=False,
+        env_prefix="BOT__",
     )
 
     bot_token: str
+    webhook_secret_token: str = ""
     admin_ids: frozenset[int] = frozenset({42, 3595399})
 
 
+# noinspection PyArgumentList
 settings = Settings()
